8910c0d7cb18c921e06fbb4bae9c0d3b8708a682
rajinitheboss <cs20b044@smail.iitm.ac.in>
2022-10-04 23:55:34 +0530

    another change

--- practice.cpp
+++ practice.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 ofstream outfile;
-outfile.open("output.txt",ios::out);
 
 int main(){
+    outfile.open("output.txt",ios::out);
     outfile<<"krishna sai kottakota"<<endl;
     outfile.close();
     return 0;
